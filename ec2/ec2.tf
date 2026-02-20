c2827435e3ef4757f1f349706d6360b5380903e2
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-20 20:14:05 +0530

    actions

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "allow_tls" { #this is for terraform reference
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"S]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
   ingress {
