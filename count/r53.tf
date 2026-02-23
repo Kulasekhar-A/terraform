7faaace33f593b5fd7fa5135a4fcb7dd3603ccd2
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-23 19:43:21 +0530

    actions

--- count/r53.tf
+++ count/r53.tf
@@ -6,3 +6,14 @@ resource "aws_route53_record" "www" {
   ttl     = 1
   records = [aws_instance.roboshop[count.index].private_ip]
 }
+
+#i want frontend as roboshop.annuru.online->public_ip
+#in this [aws_instance.roboshop[index(var.instances, "frontend")].public_ip] index is also a function
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "roboshop.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.roboshop[index(var.instances, "frontend")].public_ip]
+}
