cb2d21f04f4d4ac8bbe55398419c6e6751723c4e
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-23 19:48:43 +0530

    actions

--- for_each/r53.tf
+++ for_each/r53.tf
@@ -7,3 +7,14 @@ resource "aws_route53_record" "www" {
   records = [each.value.private_ip]
   allow_overwrite = true
 }
+
+
+resource "aws_route53_record" "www" {
+  for_each = aws_instance.roboshop
+  zone_id = var.zone_id
+  name    = "roboshop.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [lookup(aws_instance.roboshop, "frontend").public_ip]
+  allow_overwrite = true
+}
